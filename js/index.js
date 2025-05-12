fd0b81f7c295cddbfa8a82b1783c5b278b683b86
tanulo <tanulo@barossgabor.local>
2025-05-12 12:00:18 +0200

    fist commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const btnLogin = document.querySelector('.login')
+const btnReg = document.querySelector('.reg')
+
+btnLogin.addEventListener('click', ()=>{
+    window.location.href = '../html/login.html'
+
+})
+
+
+btnReg.addEventListener('click', ()=>{
+    window.location.href = '../html/registration.html'
+
+})
---
+++ js/registration.js
@@ -0,0 +1,10 @@
+const btnReg=document.querySelector('.reg')
+
+btnReg.addEventListener('click', register)
+
+async function register() {
+    const email=document.querySelector('#email').value
+    const name=document.querySelector('#name').value
+    const psw=document.querySelector('#psw').value
+    const psw2=document.querySelector('#psw2').value
+}
